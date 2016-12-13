164679609a967dd9800f422adb60bde47adaf701
XenusRUS <xenus.doc@yandex.ru>
2016-12-13 17:14:54 +0500

    9lab

---
+++ 9lab/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace lab
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Moduls.body body = new Moduls.body();
+			body.bodyOutput();
+			Console.ReadKey();
+		}
+	}
+}
